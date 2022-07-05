33d8bffd5803993541f5471419000151d1814cdb
Rafał Wawiórka <rafal.wawiorka@gmail.com>
2022-07-05 20:00:26 +0200

    Init module and created methods for creating tabs on BO

---
+++ presslytask.php
@@ -0,0 +1,148 @@
+<?php
+/**
+*
+*  @author    Rafał Wawiórka
+*/
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class Presslytask extends Module
+{
+    protected $config_form = false;
+
+    public function __construct()
+    {
+        $this->name = 'presslytask';
+        $this->tab = 'analytics_stats';
+        $this->version = '1.0.0';
+        $this->author = 'Rafał Wawiórka';
+        $this->need_instance = 0;
+        $this->bootstrap = true;
+
+        parent::__construct();
+
+        $this->displayName = $this->l('Pressly Task');
+        $this->description = $this->l('Pressly interview task');
+
+        $this->confirmUninstall = $this->l('Do you want to uninstall this module?');
+
+        $this->ps_versions_compliancy = array('min' => '1.7', 'max' => _PS_VERSION_);
+    }
+
+    public function install()
+    {
+        return parent::install() &&
+            $this->installMainTab() &&
+            $this->installTab('AdminMostExpensiveOrders', 'Most Expensive Orders') &&
+            $this->installTab('AdminWaitingOrders', 'Waiting Orders');
+    }
+
+    public function uninstall()
+    {
+        return $this->uninstallTab('AdminMostExpensiveOrders') &&
+            $this->uninstallTab('AdminWaitingOrders') &&
+            $this->uninstallMainTab() &&
+            parent::uninstall();
+    }
+
+    public function enable($force_all = false)
+    {
+        return parent::enable($force_all) &&
+            $this->installMainTab() &&
+            $this->installTab('AdminMostExpensiveOrders', 'Most Expensive Orders') &&
+            $this->installTab('AdminWaitingOrders', 'Waiting Orders');
+    }
+
+    public function disable($force_all = false)
+    {
+        return parent::disable($force_all) &&
+            $this->uninstallTab('AdminMostExpensiveOrders') &&
+            $this->uninstallTab('AdminWaitingOrders') &&
+            $this->uninstallMainTab();
+
+    }
+
+    /**
+     * This method is installing main tab for subtabs
+     * @return boolean
+     */
+    private function installMainTab() {
+        $tabId = (int) Tab::getIdFromClassName('AdminMyTab');
+        if(!$tabId) {
+            $tabId = null;
+        }
+
+        $tab = new Tab($tabId);
+        $tab->active = 1;
+        $tab->class_name = 'AdminMyTab';
+        $tab->name = array();
+        foreach (Language::getLanguages() as $lang) {
+            $tab->name[$lang['id_lang']] = $this->trans('My Tab', array(), 'Modules.PresslyTask.Admin', $lang['locale']);
+        }
+        $tab->id_parent = (int)Tab::getIdFromClassName('SELL');
+        $tab->module = '';
+
+        return $tab->save();
+    }
+
+
+    /**
+     * This method is uninstalling main "unclickable" tab
+     * @return boolean
+     */
+    private function uninstallMainTab() {
+        $tabId = (int) Tab::getIdFromClassName('AdminMyTab');
+        if (!$tabId) {
+            return true;
+        }
+
+        $tab = new Tab($tabId);
+
+        return $tab->delete();
+    }
+
+    /**
+     * This method is installing tabs on enabling or installing module
+     * @param string $className
+     * @param string $classNameTranslated
+     * @return boolean
+     */
+    private function installTab($className, $classNameTranslated)
+    {
+        $tabId = (int) Tab::getIdFromClassName($className);
+        if (!$tabId) {
+            $tabId = null;
+        }
+
+        $tab = new Tab($tabId);
+        $tab->active = 1;
+        $tab->class_name = $className;
+        $tab->name = array();
+        foreach (Language::getLanguages() as $lang) {
+            $tab->name[$lang['id_lang']] = $this->trans($classNameTranslated, array(), 'Modules.PresslyTask.Admin', $lang['locale']);
+        }
+        $tab->id_parent = (int) Tab::getIdFromClassName('AdminMyTab');
+        $tab->module = $this->name;
+
+        return $tab->save();
+    }
+
+    /**
+     * This method is uninstalling tabs then disabling or uninstalling module
+     * @param string $className
+     * @return boolean
+     */
+    private function uninstallTab($className)
+    {
+        $tabId = (int) Tab::getIdFromClassName($className);
+        if (!$tabId) {
+            return true;
+        }
+
+        $tab = new Tab($tabId);
+
+        return $tab->delete();
+    }
+}
