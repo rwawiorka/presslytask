d9b61752dd0445572a949692f9a87214a09fae15
Rafał Wawiórka <rafal.wawiorka@gmail.com>
2022-07-05 21:01:24 +0200

    Added translations

---
+++ translations/pl.php
@@ -0,0 +1,17 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{presslytask}prestashop>presslytask_65d843f181be97830b2ab836d4a9efeb'] = 'Pressly Task';
+$_MODULE['<{presslytask}prestashop>presslytask_37d7ef2070a733707fd18e4107453f4b'] = 'Zadanie rekrutacyjne dla Pressly';
+$_MODULE['<{presslytask}prestashop>presslytask_d29898f620a9dde1f0ab624309ac833a'] = 'Czy chcesz odinstalować ten moduł?';
+$_MODULE['<{presslytask}prestashop>waiting_orders_6fde42aa0857eb92fc0d0c3fb1c6c8b7'] = 'ID zamówienia';
+$_MODULE['<{presslytask}prestashop>waiting_orders_5d4710f9a8250b13164a82c94d5b00d1'] = 'Indeks';
+$_MODULE['<{presslytask}prestashop>waiting_orders_ce26601dac0dea138b7295f02b7620a7'] = 'Klient';
+$_MODULE['<{presslytask}prestashop>waiting_orders_4160dfb64fcc1a9b7e599b567b0ea1d4'] = 'Wartość zamówienia';
+$_MODULE['<{presslytask}prestashop>waiting_orders_559e7ca805230fc80e3644f87bb3994d'] = 'Data zamówienia';
+$_MODULE['<{presslytask}prestashop>most_expensive_orders_6fde42aa0857eb92fc0d0c3fb1c6c8b7'] = 'ID zamówienia';
+$_MODULE['<{presslytask}prestashop>most_expensive_orders_5d4710f9a8250b13164a82c94d5b00d1'] = 'Indeks';
+$_MODULE['<{presslytask}prestashop>most_expensive_orders_ce26601dac0dea138b7295f02b7620a7'] = 'Klient';
+$_MODULE['<{presslytask}prestashop>most_expensive_orders_4160dfb64fcc1a9b7e599b567b0ea1d4'] = 'Wartość zamówienia';
+$_MODULE['<{presslytask}prestashop>most_expensive_orders_559e7ca805230fc80e3644f87bb3994d'] = 'Data zamówienia';
